3fa13d9caaab9abf73d40f44d1a2a2441f9a672f
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2023-07-11 08:40:28 +0200

    Dodaj pliki projektów.

---
+++ DatabaseSQLTodoListApp/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseSQLTodoListApp.Models
+{
+    class TaskModel
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string Deadline { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ DatabaseSQLTodoListApp/ViewModel/AddTaskViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSQLTodoListApp.ViewModel
+{
+    public class AddTaskViewModel : ViewModelBase
+    {
+        string userName;
+        public string UserName
+        {
+            get => userName;
+            set
+            {
+                userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        DateTime deadline;
+        public DateTime Deadline
+        {
+            get => deadline;
+            set
+            {
+                deadline = value;
+                OnPropertyChanged(nameof(Deadline));
+            }
+        }
+
+
+    }
+}
